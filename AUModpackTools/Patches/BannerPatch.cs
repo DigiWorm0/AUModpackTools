81f601e9b14bef831b6d5222b361dd64e6061c82
Wael1231 <106765472+Wael1231@users.noreply.github.com>
2023-06-11 14:53:12 +0100

    Add Support TownOfModrole Mod

--- AUModpackTools/Patches/BannerPatch.cs
+++ AUModpackTools/Patches/BannerPatch.cs
@@ -19,7 +19,7 @@ namespace AUModpackTools.Patches
         {
             "MainUI/bannerLogo_AmongUs",
             "bannerLogo_TownOfUs", // Town of Us
-            "bannerLogo_TOR" // The Other Roles
+            "bannerLogo_TOR", // The Other Roles
             "bannerLogo_TOM" // Town Of Modrole
         };
 
